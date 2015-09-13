aa1c0b7f2fed96e0e28500a48c089b803d300d17
Danzante <tim-95@narod.ru>
2015-09-13 19:52:49 +0300

    Camera Control not finished
    almost done head + camera but no high locker for camera

---
+++ Gravity/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float rotationX;
+    public float rotationY;
+    private GameObject Head;
+    private GameObject Body;
+
+    // Use this for initialization
+    void Start () {
+        Head = transform.parent.gameObject;
+        Body = Head.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rotationX = Input.GetAxis("Mouse X") * 10F;
+        rotationY = Input.GetAxis("Mouse Y") * 10F;
+        if (Mathf.Abs(Vector3.Angle(Head.transform.forward, Body.transform.forward) - rotationY) < 70)
+            Head.transform.Rotate(new Vector3(-rotationY, 0, 0));
+        Body.transform.Rotate(new Vector3(0, rotationX, 0));
+    }
+}
